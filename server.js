9762d7c689642473d554341ab5065ef7ee7c9891
edilson <edilson18martins@gmail.com>
2022-07-31 00:20:05 -0300

    sec commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/TelephoneBook'));
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', "*");
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/delivery-angula/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
