eae0232966eeb9e5c5eff0f84ba78bdb246ed95f
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2022-07-31 00:37:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist/telephone-book'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/telephone-book'));
 
-app.use(function (req, res, next) {
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/delivery-angula/'}),
+);
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', "*");
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/delivery-angula/index.html'));
-});
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
