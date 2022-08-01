27dc7863998d4cb81df1eff84563d195d89abd2d
Edilson França <42967820+EdilsonFranca@users.noreply.github.com>
2022-08-01 00:18:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -8,11 +9,7 @@ const app = express();
 app.use(express.static('./dist/telephone-book'));
 
 app.get('/*', (req, res) =>
-<<<<<<< HEAD
   res.sendFile('index.html', {root: 'dist/telephone-book/'}),
-=======
-    res.sendFile('index.html', {root: 'dist/telephone-book/'}),
->>>>>>> 23b34d8c833db56ed93e3e8f0cdcc2fe181a1b7a
 );
 
 // Start the app by listening on the default Heroku port
